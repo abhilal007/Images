09bf6721f2d8a0c780345f7a94b70def2cc86856
Abhishek Lal B <abhilal007@gmail.com>
2018-03-24 16:02:29 +0530

    comments

--- scriptImage.py
+++ scriptImage.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-image = Image.open("")#fill "" with the image location
+image = Image.open("")#fill "" with the image location or name
 size = width, height = image.size
 
 dummy = image.resize((160, 300), Image.LANCZOS)
